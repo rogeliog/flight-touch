45655751d64989563a4f10391e5779f59cc002ed
Rogelio Guzman <rhuerta@twitter.com>
2013-07-08 19:51:04 -0700

    Implements event handling for simple Hammer.js events

--- test/spec/touch.spec.js
+++ test/spec/touch.spec.js
@@ -1,18 +1,41 @@
 'use strict';
 
-describeComponent('lib/touch', function () {
+describeMixin('lib/touch', function () {
 
   // Initialize the component and attach it to the DOM
   beforeEach(function () {
     setupComponent();
+    this.mixin = this.component;
   });
+
+  function shouldTriggerFlightEventBasedOnHammerEvent(flightEvent, hammerEvent) {
+    var eventSpy = spyOnEvent(document, flightEvent);
+    var element = $(document).hammer().trigger(hammerEvent);
+    expect(eventSpy).toHaveBeenTriggeredOn(document);
+  };
 
   it('should be defined', function () {
-    expect(this.component).toBeDefined();
+    expect(this.mixin).toBeDefined();
   });
 
-  it('should do something', function () {
-    expect(true).toBe(false);
+  it('should respond to Hammer tap event', function () {
+    shouldTriggerFlightEventBasedOnHammerEvent('UITap', 'tap');
+  });
+
+  it('should respond to Hammer drag event', function () {
+    shouldTriggerFlightEventBasedOnHammerEvent('UIDrag', 'drag');
+  });
+
+  it('should respond to Hammer drag event', function () {
+    shouldTriggerFlightEventBasedOnHammerEvent('UIDrag', 'drag');
+  });
+
+  it('should respond to Hammer dragstart event', function () {
+    shouldTriggerFlightEventBasedOnHammerEvent('UIDragStart', 'dragstart');
+  });
+
+  it('should respond to Hammer dragEnd event', function () {
+    shouldTriggerFlightEventBasedOnHammerEvent('UIDragEnd', 'dragend');
   });
 
 });
